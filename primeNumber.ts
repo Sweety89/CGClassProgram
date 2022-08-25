2e709d115ba77c64df9606ec50070c0f4f8bbe88
LAPTOP-1CC1AA5N\HP <sweetymondal89@gmail.com>
2022-08-25 09:58:09 +0530

    logic updated

--- primeNumber.ts
+++ primeNumber.ts
@@ -1,4 +1,18 @@
 function isPrime(num:number):boolean{
-    let isprime = false;
+    let isprime = true;
+    
+    if(num === 2) {
+        isprime = true;
+        return isprime;
+    }
+    
+    for(let i = 2 ; i < num/2 ; i++){
+        
+        //completely divisible other than 1 and number itslef meaning not prime
+        if(num%i===0){
+            isprime = false;
+            break;
+        }
+    }
     return isprime;
 }
